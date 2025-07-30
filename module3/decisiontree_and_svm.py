73fdd388ba11859b7862d9b42a7bae923d45c852
Pelaez <miguelpelaez1707@gmail.com>
2025-07-31 00:59:27 +0200

    creacion svm

---
+++ module3/decisiontree_and_svm.py
@@ -0,0 +1,41 @@
+###  build a model that predicts if a credit card transaction is fraudulent or not. ###
+
+from __future__ import print_function
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize, StandardScaler
+from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
+from sklearn.svm import LinearSVC
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Load data set
+url= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/creditcard.csv"
+df = pd.read_csv(url)
+
+labels = df.Class.unique() # Array de los valores en la columna class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
